e8ce4d7aa767d52c34c18b76fdfb661c1587eefb
Raman Ratnakar <43859527+rratnakar09@users.noreply.github.com>
2020-06-21 21:10:13 +0530

    Update urls.py

--- activityTracker/urls.py
+++ activityTracker/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('activity.urls')),
+    path('', include('activity.urls')),
 ]
